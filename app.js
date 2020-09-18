a701bdb318e5fa99ea987553649faaafe865daa0
Awet Tesfay <whitezscope@gmail.com>
2020-09-18 16:17:45 +0200

    some modification

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
+// get the input value
 const cityInput = document.getElementById("city-input").value; // get city name from user input
 
+// select the elements of html to display the daily weather data to
 const images = document.querySelectorAll(".img");
 const time = document.querySelectorAll(".time");
 const averageTemp = document.querySelectorAll(".averageTemp");
 const weatherDesElement = document.querySelectorAll(".weather-des");
 
+// select the html elements to display the current weather data, city name and the image of the city
 const imgBg = document.querySelector(".city-bg")
 const cityName = document.querySelector(".city-name");
 const curTemp = document.querySelector(".currentTemp");
 const curWeatherDes = document.querySelector(".cur-weather-des");
 const curIcon = document.querySelector(".img-current");
 
-let dayCounter = 0;
-// the api key
+// the api key and security key for the unsplash api
 const myKey = config.MY_KEY;
 const myKey_2 = config.KEY_2
 
-// change the time to the formal and readable one
+// function to change the time data to the formal and readable one
 const changeDate = (date)=> {
     let dateObj = new Date(date * 1000);
     let utcString = dateObj.toUTCString();
@@ -24,53 +26,60 @@ const changeDate = (date)=> {
     return time;
 }
 
-// data variables
+// data variables to the save the data that I get from the api
 let myData = {}
 let days = [];
 let temp = [];
 let icons = [];
 let weatherDes = [];
 
-// listen to the click event
+// listen to the click event to fetch different api
 document.getElementById("search-input").addEventListener("click", ()=> {
-    if (cityInput) {
-        // fetch data from the server
+    if (cityInput) { // check if there is input when clicked
+
+        // fetch data from the weather server to get the latitude and longitude of the city
         fetch('http://api.openweathermap.org/data/2.5/forecast?q='+cityInput+'&appid='+myKey)
-            .then(respond => respond.json()).then(weatherData => {
-            myData.name = "Today's weather of "+weatherData.city.name
-            return weatherData.city.coord;
+            .then(respond => respond.json())
+            .then(weatherData => {
+                // save the name of the city
+                myData.name = weatherData.city.name
+                return weatherData.city.coord;
         }).then(coord => {
-            // second fetch on the one call api
+            // second fetch on the one call api to get all the required weather data
             fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+coord.lat+'&lon='+coord.lon+'&units=metric&exclude=minutely,hourly&&appid='+myKey)
                 .then(respond => respond.json())
                 .then(oneCallData => {
-                    console.log(oneCallData)
-                    // current weather description
+                    console.log(oneCallData) // for testing
+                    // weather data of the at the moment
                     myData.currentTime = changeDate(oneCallData.current.dt)
                     myData.currentTemp = oneCallData.current.temp;
                     myData.feels_like = oneCallData.current.feels_like;
                     myData.description = oneCallData.current.weather[0].description;
                     myData.curIcon = oneCallData.current.weather[0].icon;
 
-                    cityName.innerHTML = myData.name
+                    // data to be displayed in the page for the current weather data
+                    cityName.innerHTML = myData.name+" "+ myData.currentTime
                     curTemp.innerHTML = Math.round(myData.currentTemp)+" °C";
                     curWeatherDes.innerHTML = myData.description
                     curIcon.setAttribute("src", 'http://openweathermap.org/img/wn/'+myData.curIcon+'@2x.png')
 
-                    // data from each day forecast
+                    // data from daily weather forecast
                     oneCallData.daily.forEach(day => {
-                        days.push(changeDate(day.dt)); // the time of each day
-                        icons.push(day.weather[0].icon) // icon for each day
+                        days.push(changeDate(day.dt));
+                        icons.push(day.weather[0].icon)
                         weatherDes.push(day.weather[0].description) // description for each day
                         let dailyAveTem = (day.temp.day + day.temp.eve + day.temp.morn + day.temp.night)/4; // calculate average temperature
                         temp.push(Math.round(dailyAveTem)+" °C");
                     })
-                    // data in one weather variable of object
+                    // save data in one weather variable of object
                     myData.dailyAveTem = temp;
                     myData.icon = icons;
                     myData.weatherDes = weatherDes;
                     myData.days = days
 
+                    // used to index each data to the corresponding day, I skipped the data for the current day because
+                    // I display the current weather instead in it
+                    let dayCounter = 0;
                     images.forEach(img => {
                         img.setAttribute('src', 'http://openweathermap.org/img/wn/'+icons[dayCounter+1]+'@2x.png');
                         averageTemp[dayCounter].innerHTML = temp[dayCounter+1];
@@ -78,20 +87,21 @@ document.getElementById("search-input").addEventListener("click", ()=> {
                         weatherDesElement[dayCounter].innerHTML = weatherDes[dayCounter+1]
                         dayCounter++;
                     })
-                    document.querySelector(".city-name").innerHTML  = myData.name
                     dayCounter = 0;
+
+                    // another fetch for the image of the city from unsplash api
+                    fetch('https://api.unsplash.com/search/photos?query='+cityInput+'&client_id='+myKey_2)
+                        .then(respond => respond.json())
+                        .then(imageData => {
+                            imgBg.style.backgroundImage = 'url('+imageData.results[0].urls.small+')';
+                            imgBg.style.backgroundRepeat = "no-repeat"
+                            imgBg.style.backgroundPosition = "center"
+                        })
                 }).catch(error => {
                     alert("Enter correct city name")
             })
         }).catch(error => {
             alert("Enter correct city name")
         })
-        fetch('https://api.unsplash.com/search/photos?query='+cityInput+'&client_id='+myKey_2)
-            .then(respond => respond.json())
-            .then(imageData => {
-                imgBg.style.backgroundImage = 'url('+imageData.results[0].urls.small+')';
-                imgBg.style.backgroundRepeat = "no-repeat"
-                imgBg.style.backgroundPosition = "center"
-            })
     }
 });
